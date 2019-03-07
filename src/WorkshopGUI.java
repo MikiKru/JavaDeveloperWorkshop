77b8fe35296ed47e6647a03cc7934b5b63725cfe
MikiKru <michal_kruczkowski@o2.pl>
2019-03-07 18:38:33 +0100

    commit 1

---
+++ src/WorkshopGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+// psvm -> public static void main(){}
+// ALT + ENTER -> auto-podpowiedź
+// CTRL + SHIFT + F10 - run
+public class WorkshopGUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Naucz się programowania");
+
+        frame.setSize(300,300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
