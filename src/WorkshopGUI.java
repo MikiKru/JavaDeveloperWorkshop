6833dc07e38c1e6b18655e08d2a9c7360f4db5af
MikiKru <michal_kruczkowski@o2.pl>
2019-03-07 18:57:34 +0100

    commit 2

--- src/WorkshopGUI.java
+++ src/WorkshopGUI.java
@@ -1,11 +1,25 @@
 import javax.swing.*;
+// https://github.com/MikiKru/JavaDeveloperWorkshop
 
 // psvm -> public static void main(){}
 // ALT + ENTER -> auto-podpowiedź
 // CTRL + SHIFT + F10 - run
+// CTRL + D -> duplicate
 public class WorkshopGUI {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Naucz się programowania");
+        // ----------------------------------------------------
+        JLabel lblName = new JLabel("Imię i nazwisko");
+        lblName.setBounds(20, 20, 150, 20);
+        JTextField tfName = new JTextField();
+        tfName.setBounds(20, 50, 150, 20);
+        // ----------------------------------------------------
+        JLabel lblEmail = new JLabel("Email");
+        lblEmail.setBounds(20, 80, 150, 20);
+        JTextField tfEmail = new JTextField();
+        tfEmail.setBounds(20,110, 150, 20);
+        // ----------------------------------------------------
+
 
         frame.setSize(300,300);
         frame.setLayout(null);
